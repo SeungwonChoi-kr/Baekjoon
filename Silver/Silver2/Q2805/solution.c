61c1c3aa97e1ddec4f0ef2915275843d146f9851
SeungwonChoi-kr <choiswon2002@gmail.com>
2025-03-13 02:12:10 +0900

    solve Q2805

--- Silver/Silver2/Q2805/solution.c
+++ Silver/Silver2/Q2805/solution.c
@@ -4,20 +4,22 @@
 
 void mergeSort(int* arr, int start, int end);
 void merge(int* arr, int start, int middle, int end);
-int cutting_tree(int* trees, int tree_num, int cutter_length, int need_length);
+long check_remainder(int* trees, long need_length, long s, long e, long tree_num);
+int cutting_tree(int* trees, long saw_length, long need_length, long tree_num);
 
 int main() {
-    int tree_num, need_length, cutting_length;
-    scanf("%d %d", &tree_num, &need_length);
+    long tree_num, need_length;
+    long saw_length;
+    scanf("%ld %ld", &tree_num, &need_length);
 
     int* trees = (int*)malloc(tree_num * sizeof(int));
     for (int i = 0; i < tree_num; i++) {
         scanf("%d", &trees[i]);
     }
-
     mergeSort(trees, 0, tree_num - 1);
-    cutting_length = cutting_tree(trees, tree_num, (trees[0] + trees[tree_num - 1]) / 2, need_length);
-    printf("%d", cutting_length);
+
+    saw_length = check_remainder(trees, need_length, 0, trees[0], tree_num);
+    printf("%ld", saw_length);
 
     return 0;
 }
@@ -57,27 +59,58 @@ void merge(int* arr, int start, int middle, int end) {
     for (int k = start; k <= end; k++) {
         arr[k] = copy[idx++];
     }
+    free(copy);
 }
 
-int cutting_tree(int* trees, int tree_num, int cutter_length, int need_length) {
-    int cut_length = 0;
-    bool overCut = false;
+long check_remainder(int* trees, long need_length, long s, long e, long tree_num) {
+    long p = 0, result = 0;
 
-    for (int i = 0; i < tree_num; i++) {
-        cut_length += trees[i] - cutter_length;
+    while (s + 1 < e) {
+        p = (s + e) / 2;
+        result = cutting_tree(trees, p, need_length, tree_num);
 
-        if (cut_length > need_length) {
-            overCut = true;
+        // printf("(%ld, %ld) r[%ld]: %ld\n", s, e, p, result);
 
-            if (i + 1 == tree_num) {
-                overCut = false;
-            }
+        if (result > 0) {
+            s = p;
+        } else if (result == 0) {
+            s = p;
+        } else {
+            e = p - 1;
         }
     }
 
-    if (cut_length > need_length) {
-        cutting_tree(trees, tree_num, cutter_length / 2, need_length);
+    if (result < 0) {
+        p--;
+
+        if (cutting_tree(trees, p, need_length, tree_num) < 0) {
+            p--;
+        }
+    } else if (result > 0) {
+        if (cutting_tree(trees, p + 1, need_length, tree_num) >= 0) {
+            p++;
+        }
     }
 
-    return 0;
+    return p;
+}
+
+int cutting_tree(int* trees, long saw_length, long need_length, long tree_num) {
+    long remainder = 0;
+    for (int i = 0; i < tree_num; i++) {
+        if (trees[i] - saw_length > 0) {
+            remainder += trees[i] - saw_length;
+        } else {
+            break;
+        }
+    }
+    remainder -= need_length;
+
+    if (remainder > 0) {
+        remainder = 1;
+    } else if (remainder < 0) {
+        remainder = -1;
+    }
+
+    return remainder;
 }
